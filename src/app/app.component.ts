3cfc9d814ab4aa1923912c1ae080c0cba0bd6906
Brent Engelbrecht <brent.engelbrecht@zinkworks.com>
2023-03-19 22:49:10 +0000

    Add inter-component messaging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-book';
+  loadPage = 'recipe';
+
+  onNavigate(page: string) {
+    this.loadPage = page;
+  }
 }
